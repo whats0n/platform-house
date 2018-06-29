d29036407fbc86f8e5415a5ee560a760cfd7f6f6
whats0n <mrdvv@mail.ru>
2018-06-29 14:29:14 +0300

    update texts and small fixes

--- src/js/components/logo.js
+++ src/js/components/logo.js
@@ -2,10 +2,11 @@ import {WIN, ACTIVE} from '../constants';
 
 const logo = $('.js-logo');
 
-let start = WIN.scrollTop();
+// let start = WIN.scrollTop();
 
 WIN.on('load scroll', () => {
-  let current = WIN.scrollTop();
-  start < current ? logo.addClass(ACTIVE) : logo.removeClass(ACTIVE);
-  start = current;
+  // let current = WIN.scrollTop();
+  WIN.scrollTop() > 0 ? logo.addClass(ACTIVE) : logo.removeClass(ACTIVE);
+  // start < current ? logo.addClass(ACTIVE) : logo.removeClass(ACTIVE);
+  // start = current;
 });
